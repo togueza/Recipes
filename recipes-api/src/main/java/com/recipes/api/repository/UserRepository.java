7ebd69cf787dda63054db9b11ae74239983f9b1a
togueza <togueza@gmail.com>
2023-09-21 02:21:06 -0600

    Delete recipes-api directory

--- recipes-api/src/main/java/com/recipes/api/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.recipes.api.repository;
-
-import com.recipes.api.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
--- recipes-api/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into recipes(id, name, description, super_powers)
-values (1, 'Hulk', 'The green gemn', 'Strength, Resistance, Gama radiation');
-insert into recipes(id, name, description, super_powers)
-values (2, 'Iron Man', 'A smart man with an armor', 'Flying, shot, laser, resistance');
