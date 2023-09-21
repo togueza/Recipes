86760ac608f252107b88a935c6f7746b382d77a6
togueza <togueza@gmail.com>
2023-09-21 02:36:59 -0600

    Delete recipes-app directory

--- recipes-app/src/main/java/com/recipes/app/repository/RecipesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.recipes.app.repository;
-
-import com.recipes.app.entity.Recipes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipesRepository extends JpaRepository<Recipes, Long> {
-
-}
--- recipes-app/src/main/java/com/recipes/app/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.recipes.app.repository;
-
-import com.recipes.app.entity.UserTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserTable, String> {
-
-    UserTable findByUsername(String username);
-}
